65a6f289a593a2c5add9726ff0eead37c59b26d3
gengadharan4656 <gengadharan4656s@gmail.com>
2026-02-22 19:01:45 +0530

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.pdf_manager_pro"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.pdf_manager_pro"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+    release {
+        signingConfig = signingConfigs.getByName("debug")
+
+        // 🔥 Disable R8 / ProGuard (fixes build crash)
+        isMinifyEnabled = false
+        isShrinkResources = false
+    }
+}
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+}
+flutter {
+    source = "../.."
+}
---
+++ android/app/src/main/kotlin/com/example/pdf_manager_pro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pdf_manager_pro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
